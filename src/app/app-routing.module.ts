20bd2db66e3032a081d7e11052455d968a17c7a4
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-23 00:27:48 +0100

    Renamed routes constant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { VerifyEmailComponent } from './core/verify-email/verify-email.component
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './error-pages/page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
-import { ROUTES } from './shared/constants/app-routing.constants';
+import { APP_ROUTES } from './shared/constants/app-routing.constants';
 import { AboutComponent } from './static-pages/about/about.component';
 import { ContactComponent } from './static-pages/contact/contact.component';
 import { TermsOfUseComponent } from './static-pages/terms-of-use/terms-of-use.component';
@@ -26,49 +26,49 @@ const routes: Routes = [
   // *****************************************
   // * User auth, registration and validation
   {
-    path: ROUTES.LOGIN,
+    path: APP_ROUTES.LOGIN,
     component: LoginComponent,
   },
   {
-    path: ROUTES.REGISTER,
+    path: APP_ROUTES.REGISTER,
     component: RegisterComponent,
   },
   {
-    path: ROUTES.REGISTER_COMPLETE,
+    path: APP_ROUTES.REGISTER_COMPLETE,
     component: RegistrationCompleteComponent,
   },
   {
-    path: ROUTES.VERIFY_EMAIL,
+    path: APP_ROUTES.VERIFY_EMAIL,
     component: VerifyEmailComponent,
   },
   {
-    path: ROUTES.RESET_PASSWORD,
+    path: APP_ROUTES.RESET_PASSWORD,
     component: ResetPasswordRequestComponent,
   },
   {
-    path: ROUTES.CHANGE_PASSWORD,
+    path: APP_ROUTES.CHANGE_PASSWORD,
     component: ChangePasswordComponent,
   },
 
   // *****************************************
   // * Static pages
   {
-    path: ROUTES.ABOUT,
+    path: APP_ROUTES.ABOUT,
     component: AboutComponent,
   },
   {
-    path: ROUTES.CONTACT,
+    path: APP_ROUTES.CONTACT,
     component: ContactComponent,
   },
   {
-    path: ROUTES.TERMS_OF_USE,
+    path: APP_ROUTES.TERMS_OF_USE,
     component: TermsOfUseComponent,
   },
 
   // *****************************************
   // * STO App routes
   {
-    path: ROUTES.STO_DASHBOARD,
+    path: APP_ROUTES.STO_DASHBOARD,
     component: DashboardComponent,
     canActivate: [AuthGuard],
   },
