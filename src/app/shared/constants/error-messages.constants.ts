05f75d17a3a5922e805368c6a8aeb6203e89626e
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-31 01:25:44 +0100

    Added HTTP 400 error message constants

--- src/app/shared/constants/error-messages.constants.ts
+++ src/app/shared/constants/error-messages.constants.ts
@@ -9,7 +9,9 @@ import {
 
 // API Response error messages
 export const MSG_ERROR_HTTP_STATUS_0_DISPLAY_TEXT = `Subspace frequencies are getting jammed, and we cannot connect to the server. Please try again later.`;
-export const MSG_ERROR_HTTP_STATUS_0_CONSOLE_TEXT = `Login error: Could not connect to the server.`;
+export const MSG_ERROR_HTTP_STATUS_0_CONSOLE_TEXT = `Error: Could not connect to the server.`;
+export const MSG_ERROR_HTTP_STATUS_400_DISPLAY_TEXT = `The Starbase had issues processing your request. Please try again later.`;
+export const MSG_ERROR_HTTP_STATUS_400_CONSOLE_TEXT = `Error: Bad request response.`;
 
 export const MSG_ERROR_INVALID_LOGIN_DISPLAY_TEXT = `Unauthorised: Invalid email or password.`;
 export const MSG_ERROR_EMAIL_NOT_VERIFIED_DISPLAY_TEXT = `Please verify your email before logging in. Check your inbox for the verification email.`;
