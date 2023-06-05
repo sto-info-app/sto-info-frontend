47fa56b6be07aee3f2f0a15bdc996424a955093e
Steve Roberts <steve@shadowcomputers.co.uk>
2023-06-05 03:52:55 +0100

    Profile page init

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, ProfileComponent],
   imports: [CommonModule, SharedModule],
   exports: [DashboardComponent],
 })
