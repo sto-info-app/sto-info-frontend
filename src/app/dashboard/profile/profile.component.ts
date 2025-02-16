1e90cd86b65466dae9b4d6817f08f231a22ad109
Steve Roberts <steve@shadowcomputers.co.uk>
2025-02-16 03:16:09 +0000

    refactor: Changed to using optional chain expression instead, as it's more concise and easier to read

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -37,17 +37,17 @@ export class ProfileComponent {
   }
 
   timeSinceLastLogin(): string {
-    if (!this.user || !this.user.lastLogin) return 'Unknown';
+    if (!this.user?.lastLogin) return 'Unknown';
     return this.dateTimeHelper.timeSince(this.user.lastLogin);
   }
 
   timeSinceLastPasswordReset(): string {
-    if (!this.user || !this.user.lastPasswordReset) return 'Unknown';
+    if (!this.user?.lastPasswordReset) return 'Unknown';
     return this.dateTimeHelper.timeSince(this.user.lastPasswordReset);
   }
 
   timeSinceLastUpdated(): string {
-    if (!this.user || !this.user.updatedAt) return 'Unknown';
+    if (!this.user?.updatedAt) return 'Unknown';
     return this.dateTimeHelper.timeSince(this.user.updatedAt);
   }
 }
