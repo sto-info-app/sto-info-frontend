461774d6d3fe7eb2118502ccc50d226269381947
Steve Roberts <steve@shadowcomputers.co.uk>
2025-02-16 23:53:55 +0000

    chore: Added new constants for additional clientside checks

--- src/app/shared/constants/regex-patterns.constants.ts
+++ src/app/shared/constants/regex-patterns.constants.ts
@@ -1,12 +1,18 @@
-import { MIN_CHARS_PASSWORD } from './forms.constants';
+import {
+  MAX_CHARS_PASSWORD,
+  MAX_CHARS_USERNAME,
+  MIN_CHARS_PASSWORD,
+  MIN_CHARS_USERNAME,
+} from './forms.constants';
 
 export const EMAIL_PATTERN =
   // eslint-disable-next-line no-useless-escape
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-export const USERNAME_PATTERN = '^[a-zA-Z0-9]*$';
+export const USERNAME_PATTERN = new RegExp(
+  `^[a-zA-Z0-9]{${MIN_CHARS_USERNAME},${MAX_CHARS_USERNAME}}$`,
+);
 
-export const PASSWORD_PATTERN =
-  '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9\n\r\t]).{' +
-  MIN_CHARS_PASSWORD +
-  ',}$';
+export const PASSWORD_PATTERN = new RegExp(
+  `^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9\n\r\t]).{${MIN_CHARS_PASSWORD},${MAX_CHARS_PASSWORD}}$`,
+);
