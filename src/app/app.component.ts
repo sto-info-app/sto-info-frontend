1c223919906c95e187195441f0c7cd7fcec3b8f3
Steve Roberts <steve@shadowcomputers.co.uk>
2025-03-09 15:49:53 +0000

    test: moved event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -198,23 +198,23 @@ export class AppComponent {
         console.log('CookieYes script loaded.');
         if ((window as { CookieYes?: { run: () => void } }).CookieYes) {
           (window as { CookieYes?: { run: () => void } }).CookieYes?.run(); // Trigger manual load
-
-          console.log('CookieYes script run. Listening for consent update...');
-          // Listen for the cookie consent update event
-          window.addEventListener('cookie-consent-update', event => {
-            console.log('Event Listener: Cookie consent update event:', event);
-            const customEvent = event as CustomEvent<{ consented: boolean }>;
-            console.log('Consent update:', customEvent.detail);
-            if (customEvent?.detail?.consented) {
-              this.consentGiven();
-            } else {
-              this.consentDenied();
-            }
-          });
-
-          // Also handle existing cookie consent state on load
-          this.checkConsentState();
         }
+
+        console.log('CookieYes script run. Listening for consent update...');
+        // Listen for the cookie consent update event
+        window.addEventListener('cookie-consent-update', event => {
+          console.log('Event Listener: Cookie consent update event:', event);
+          const customEvent = event as CustomEvent<{ consented: boolean }>;
+          console.log('Consent update:', customEvent.detail);
+          if (customEvent?.detail?.consented) {
+            this.consentGiven();
+          } else {
+            this.consentDenied();
+          }
+        });
+
+        // Also handle existing cookie consent state on load
+        this.checkConsentState();
       };
 
       script.onerror = () => {
