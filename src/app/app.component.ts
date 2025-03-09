22c11c9e1e080bc97488a2e38257ed24055e4266
Steve Roberts <steve@shadowcomputers.co.uk>
2025-03-09 21:47:27 +0000

    test: added console log debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -391,14 +391,19 @@ export class AppComponent implements OnInit, OnDestroy {
   // }
 
   extractAcceptedConsentCookieCategories(cookieValue: string): string[] {
+    console.log('Extracting accepted consent cookie categories:', cookieValue);
+
     // Check if the cookie string contains the cookieyes-consent cookie
     const cookieString = cookieValue
       .split('; ')
       .find(row => row.startsWith(`${this.cookieYesCookieName}=`));
 
     if (cookieString) {
+      console.log('Cookie string:', cookieString);
       const cookieValue = cookieString.split('=')[1];
+      console.log('Cookie value:', cookieValue);
       const cookieParts = cookieValue.split(',');
+      console.log('Cookie parts:', cookieParts);
 
       const acceptedCategories = cookieParts
         .filter(part => {
@@ -409,9 +414,11 @@ export class AppComponent implements OnInit, OnDestroy {
         })
         .map(part => part.split(':')[0]);
 
+      console.log('Accepted categories:', acceptedCategories);
       this.cookieService.setUserAcceptedCookieCategories(acceptedCategories);
     }
     const categories = cookieValue.split(',');
+    console.log('Extracted categories:', categories);
     return categories;
   }
 }
