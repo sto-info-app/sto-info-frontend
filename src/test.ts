3b4a4e5c3e7b5f255443fe0959db61195e424ff0
Steve Roberts <steve@shadowcomputers.co.uk>
2025-12-08 05:37:18 +0000

    refactor: remove debug logging and streamline spec loading for clarity

--- src/test.ts
+++ src/test.ts
@@ -14,27 +14,3 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
-
-// --- DEBUG: log which spec is running ---
-// const jasmineEnv = (window as any).jasmine.getEnv();
-// let specCounter = 0;
-
-// jasmineEnv.addReporter({
-//   specStarted(result: any) {
-//     specCounter += 1;
-//     // eslint-disable-next-line no-console
-//     console.log(`SPEC START [${specCounter}] ${result.fullName}`);
-//   },
-//   specDone(result: any) {
-//     // eslint-disable-next-line no-console
-//     console.log(
-//       `SPEC DONE  [${specCounter}] ${result.fullName} - ${result.status}`,
-//     );
-//   },
-// });
-
-// // Loads all specs:
-// const context = require.context('./', true, /\.spec\.ts$/);
-// for (const key of context.keys()) {
-//   context(key);
-// }
