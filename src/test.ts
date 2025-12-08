803da7d8ec228bfa814ce425e7c0591ed8661992
Steve Roberts <steve@shadowcomputers.co.uk>
2025-12-08 04:17:18 +0000

    refactor: add Jasmine reporter for better spec logging

--- src/test.ts
+++ src/test.ts
@@ -14,3 +14,26 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+
+// --- DEBUG: log which spec is running ---
+const jasmineEnv = (window as any).jasmine.getEnv();
+let specCounter = 0;
+
+jasmineEnv.addReporter({
+  specStarted(result: any) {
+    specCounter += 1;
+    // eslint-disable-next-line no-console
+    console.log(`SPEC START [${specCounter}] ${result.fullName}`);
+  },
+  specDone(result: any) {
+    // eslint-disable-next-line no-console
+    console.log(
+      `SPEC DONE  [${specCounter}] ${result.fullName} - ${result.status}`,
+    );
+  },
+});
+// --- END DEBUG ---
+
+// Then the usual Angular CLI bit that loads all specs:
+const context = require.context('./', true, /\.spec\.ts$/);
+context.keys().map(context);
