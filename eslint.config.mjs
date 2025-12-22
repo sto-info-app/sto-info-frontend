aa7c9c48911199e752f61d6c035182ced3b26174
Steve Roberts <steve@shadowcomputers.co.uk>
2025-12-22 01:58:13 +0000

    chore(eslint): remove old ESLint configuration and add new config file

---
+++ eslint.config.mjs
@@ -0,0 +1,110 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import angularEslintTemplate from "@angular-eslint/eslint-plugin-template";
+import parser from "@angular-eslint/template-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["src/index.html"]), {
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+    },
+}, {
+    files: ["**/*.ts"],
+    extends: compat.extends("plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            project: ["tsconfig.eslint.json"],
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+    },
+}, {
+    files: ["**/*.html"],
+    extends: compat.extends("plugin:@angular-eslint/template/recommended"),
+
+    plugins: {
+        "@angular-eslint/template": angularEslintTemplate,
+    },
+
+    languageOptions: {
+        parser: parser,
+    },
+
+    rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+    },
+}, {
+    files: ["src/environments/inject-env-vars.js"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-require-imports": "off",
+        "no-undef": "off",
+    },
+}, {
+    files: ["**/jest.config.cjs"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-require-imports": "off",
+        "no-undef": "off",
+    },
+}]);
