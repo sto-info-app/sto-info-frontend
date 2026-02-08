74fe523eeee2aedaca924f1d77d231c491d24896
Steve Roberts <steve@shadowcomputers.co.uk>
2026-02-08 23:52:49 +0000

    Feat: Implement 'About the Team' developers pages

---
+++ src/app/static-pages/team/developers/developers.component.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TEST_SUPPORTERS, TEST_TEAM_MEMBERS } from '../testing/team-test-data';
+
+import { TeamDevelopersComponent } from './developers.component';
+
+jest.mock('../team.data', () => {
+  return {
+    TEAM_MEMBERS: TEST_TEAM_MEMBERS,
+    SUPPORTERS: TEST_SUPPORTERS,
+  };
+});
+
+describe('TeamDevelopersComponent', () => {
+  let component: TeamDevelopersComponent;
+  let fixture: ComponentFixture<TeamDevelopersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TeamDevelopersComponent, RouterTestingModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TeamDevelopersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should list current and past developers', () => {
+    expect(component.currentMembers.length).toBeGreaterThan(0);
+    expect(component.pastMembers.length).toBeGreaterThan(0);
+  });
+
+  it('should build thumbnail urls', () => {
+    const photoUrl = 'https://example.com/photo';
+
+    expect(component.getThumbnailUrl(photoUrl)).toBe(
+      `${photoUrl}/${component.photoVariant}`,
+    );
+    expect(component.getThumbnailUrl()).toBe(component.fallbackPhotoUrl);
+  });
+});
