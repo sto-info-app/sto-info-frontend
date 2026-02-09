5e842b068edb65ca1141296ae3b4641beb0bc312
Steve Roberts <steve@shadowcomputers.co.uk>
2026-02-09 16:54:57 +0000

    refactor(team): improve TeamSupportersComponent test setup and assertions

--- src/app/static-pages/team/supporters/supporters.component.spec.ts
+++ src/app/static-pages/team/supporters/supporters.component.spec.ts
@@ -1,7 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { TEST_SUPPORTERS, TEST_TEAM_MEMBERS } from '../testing/team-test-data';
 import { TeamSupportersComponent } from './supporters.component';
+
+jest.mock('../team.data', () => {
+  return {
+    TEAM_MEMBERS: TEST_TEAM_MEMBERS,
+    SUPPORTERS: TEST_SUPPORTERS,
+  };
+});
 
 describe('TeamSupportersComponent', () => {
   let component: TeamSupportersComponent;
@@ -22,7 +30,12 @@ describe('TeamSupportersComponent', () => {
   });
 
   it('should list current and past supporters', () => {
-    expect(component.currentSupporters).toEqual(expect.any(Array));
-    expect(component.pastSupporters).toEqual(expect.any(Array));
+    expect(component.currentSupporters.length).toBeGreaterThan(0);
+    expect(component.pastSupporters.length).toBeGreaterThan(0);
+  });
+
+  it('should get route link', () => {
+    const route = 'test-route';
+    expect(component.getRouteLink(route)).toContain(route);
   });
 });
